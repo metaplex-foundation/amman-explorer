da8d477df32668bb1a1bbd882839b5c2edfd03d0
Thorsten Lorenz <thlorenz@gmx.de>
2022-04-04 09:00:19 -0600

    guide: full command line example

--- src/amman/pages/AmmanGuidePage.tsx
+++ src/amman/pages/AmmanGuidePage.tsx
@@ -7,15 +7,14 @@ import { ClusterStatus, useCluster } from "../../providers/cluster";
 export function AmmanGuidePage() {
   // TODO(thlorenz): what do we do if another cluster is selected here?
   const { status } = useCluster();
-  const  ammanConnected = status === ClusterStatus.Connected
+  const ammanConnected = status === ClusterStatus.Connected;
   return (
     <div className="container mt-4">
-      <AmmanConnectionGreeting
-        ammanConnected={ammanConnected}
-      />
+      <AmmanConnectionGreeting ammanConnected={ammanConnected} />
       <h2 className="mt-4">Guide</h2>
       <Installation ammanConnected={ammanConnected} />
       <Usage />
+      <CliExample />
     </div>
   );
 }
@@ -116,7 +115,9 @@ function Usage() {
           </td>
         </tr>
         <tr>
-          <td>3. Airdrop to the Wallet and label it <em>wallet</em></td>
+          <td>
+            3. Airdrop to the Wallet and label it <em>wallet</em>
+          </td>
           <td className="text-lg-end">
             <Copyable text={airdrop}>
               <code>{airdrop}</code>
@@ -124,7 +125,13 @@ function Usage() {
           </td>
         </tr>
         <tr>
-          <td>4. Discover more Amman Commands</td>
+          <td>4. Inspect transaction in Amman Explorer</td>
+          <td className="text-lg-end">
+            <em>It'll show up under "Recent Transactions" above</em>
+          </td>
+        </tr>
+        <tr>
+          <td>5. Discover more Amman Commands</td>
           <td className="text-lg-end">
             <Copyable text={ammanHelp}>
               <code>{ammanHelp}</code>
@@ -132,7 +139,9 @@ function Usage() {
           </td>
         </tr>
         <tr>
-          <td>5. When finished Stop Amman (<em>Ctrl-C also works</em>)</td>
+          <td>
+            6. When finished Stop Amman (<em>Ctrl-C also works</em>)
+          </td>
           <td className="text-lg-end">
             <Copyable text={ammanStop}>
               <code>{ammanStop}</code>
@@ -143,3 +152,76 @@ function Usage() {
     </div>
   );
 }
+
+function CliExample() {
+  return (
+    <div className="card">
+      <div className="card-header align-items-center">
+        <h3 className="card-header-title">Full Command Line Example</h3>
+      </div>
+      <TableCardBody>
+      <p className="m-3">Follow <Link to={walkThruUrl}>this walk through</Link>
+        <span>or run the script below</span>
+      </p>
+            <Copyable text={CLI_EXAMPLE}>
+      <pre className="m-3 p-3" >
+        {CLI_EXAMPLE}
+      </pre>
+      </Copyable>
+    </TableCardBody>
+    </div>
+  );
+}
+// -----------------
+// CLI Example
+// -----------------
+
+const walkThruUrl =
+  '"https://gist.github.com/thlorenz/0a4fd8afe81dc1ba81e63619ed5a77bf#file-walk-thru-md';
+
+const CLI_EXAMPLE = `
+#!/usr/bin/env bash
+
+## Prepare Wallets
+
+solana-keygen new --silent --no-bip39-passphrase --outfile solpair1.json 
+solana-keygen new --silent --no-bip39-passphrase --outfile solpair2.json 
+
+### Airdrop and label them
+
+amman airdrop ./solpair1.json -l soladdr1
+amman airdrop ./solpair2.json -l soladdr2
+
+### Use first one as default wallet
+
+solana config set --keypair ./solpair1.json
+
+amman account soladdr1
+
+## Token Creation
+
+amman run -l token1 -l tx-create-token1 -- \
+  spl-token create-token --mint-authority ./solpair1.json
+
+## Token Account Creation
+
+amman run -l ata-tok1-addr1 -l tx-create-ata-tok1-addr1 -- \
+  spl-token create-account +token1 --owner +soladdr1
+
+## Token Minting
+
+amman run -l tx-mint-token1-to-ata-tok1-addr1 -t -- \
+  spl-token mint +token1 10 +ata-tok1-addr1 --mint-authority ./solpair1.json
+
+## Token Transferring
+
+### Create ATA for 'soladdr2'
+
+amman run -l ata-tok1-addr2 -l tx-ata-tok1-addr2 -- \
+  spl-token create-account +token1 --owner +soladdr2
+
+### Transfer the token Soladdr2
+
+amman run -l tx-transfer-token1-addr2 -t -- \
+  spl-token transfer +token1 1 +soladdr2 --owner ./solpair1.json
+`;
