a3a9126e9a933cdde66029d6fbefa8dbb270768b
Thorsten Lorenz <thlorenz@gmx.de>
2022-04-24 18:13:32 -0500

    chore: switch to hash router

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import "./scss/theme-dark.scss";
 import App from "./App";
 import { ClusterProvider } from "./providers/cluster";
